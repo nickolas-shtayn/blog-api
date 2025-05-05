1ab34796b413aafd0b3d04cd456c09a7a10469ac
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-05-05 11:00:50 -0500

    Add jwt functionality and status codes

---
+++ drizzle/0000_chubby_obadiah_stane.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "posts" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "posts_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" text NOT NULL,
+	"content" text NOT NULL,
+	"user_id" integer
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"email" text NOT NULL,
+	"password" text NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "posts" ADD CONSTRAINT "posts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import { db } from "./db/index.js";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import { eq } from "drizzle-orm";
-import { v4 as uuidv4 } from "uuid";
-import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
 
 const server = express();
@@ -18,17 +16,26 @@ server.use(
   })
 );
 server.use(express.json());
-server.use(cookieParser());
+
+const extractUserFromToken = function (req, res, next) {
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  req.user = decoded;
+
+  next();
+};
 
 server.get("/", async (req, res) => {
   const allPosts = await db.select().from(posts);
 
-  res.json(allPosts);
+  res.status(200).json(allPosts);
 });
 
-server.post("/", async (req, res) => {
-  const post = await db.insert(posts).values(req.body);
-  res.send("ok");
+server.post("/", extractUserFromToken, async (req, res) => {
+  const { name, content } = req.body;
+  const userId = req.user.sub;
+  const post = await db.insert(posts).values({ name, content, userId });
+  res.status(200).send("post added to user");
 });
 
 server.post("/login", async (req, res) => {
@@ -48,28 +55,28 @@ server.post("/login", async (req, res) => {
       const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      res.cookie("jwt", token, {
-        httpOnly: true,
-        sameSite: "strict",
-        maxAge: 360000,
-        path: "/",
-      });
-      res.send("authenticated");
+      res.send(token);
     } else {
-      res.send("wrong password");
+      res.status(401).send("wrong password");
     }
   } else {
-    res.send("wrong email");
+    res.status(401).send("wrong email");
   }
 });
 
 server.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
-  const hash = await bcrypt.hash(password, 13);
-  const signUp = await db.insert(users).values({ email, password: hash });
+  const user = await db.select().from(users).where(eq(users.email, email));
 
-  res.send("signed up");
+  if (user.length === 0) {
+    const hash = await bcrypt.hash(password, 13);
+    const signUp = await db.insert(users).values({ email, password: hash });
+
+    res.send("signed up");
+  } else {
+    res.status(409).send("This email address is already registered");
+  }
 });
 
 server.listen(PORT, () => {
